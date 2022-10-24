65e3127b5e93682d0b4c8a917ea2541feb6c2606
hwang434 <inqbator@knou.ac.kr>
2022-10-24 14:01:06 +0900

    First init

---
+++ app/src/main/java/com/hig/iga_works/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hig.iga_works;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ iga_works_sdk/src/main/java/com/hig/iga_works_sdk/IGASDK.java
@@ -0,0 +1,20 @@
+package com.hig.iga_works_sdk;
+
+import java.util.Dictionary;
+
+public class IGASDK {
+    // SDK를 초기화 합니다. appkey가 필수적으로 필요합니다.
+    public static void init(String appkey) {
+
+    }
+
+    // 사용자 속성을 입력합니다.
+    public static void setUserProperty(Dictionary<String,Object> keyValue) {
+
+    }
+
+    // 이벤트를 입력합니다.
+    public static void addEvent(String eventName, Dictionary<String, Object> keyValue) {
+
+    }
+}
