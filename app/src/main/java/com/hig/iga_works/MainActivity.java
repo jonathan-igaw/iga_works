2b2ad2d09d2de2a8ff92ea3ed264a4e57649b26d
hwang434 <inqbator@knou.ac.kr>
2022-10-24 14:16:42 +0900

    Add Logging

--- app/src/main/java/com/hig/iga_works/MainActivity.java
+++ app/src/main/java/com/hig/iga_works/MainActivity.java
@@ -3,13 +3,15 @@ package com.hig.iga_works;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
