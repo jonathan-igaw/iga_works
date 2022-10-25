e98419e94c2455dd5af5ea53aa34fe137bd77092
hwang434 <inqbator@knou.ac.kr>
2022-10-25 14:27:34 +0900

    addEvent 기능이랑 테스트 코드 추가 addEvent 메서드를 통해 api 요청 성공. junit으로 테스트 코드도 추가하였습니다.

--- iga_works_sdk/src/main/java/com/hig/iga_works_sdk/IGASDK.java
+++ iga_works_sdk/src/main/java/com/hig/iga_works_sdk/IGASDK.java
@@ -1,20 +1,93 @@
 package com.hig.iga_works_sdk;
 
+import android.util.Log;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Dictionary;
 
 public class IGASDK {
+    private static final String TAG = "IGASDK";
+    private static final String DOMAIN = "http://adbrix-sdk-assignment-backend-115895936.ap-northeast-1.elb.amazonaws.com";
+
     // SDK를 초기화 합니다. appkey가 필수적으로 필요합니다.
     public static void init(String appkey) {
-
+        Log.d(TAG, "init: appkey = "+appkey);
     }
 
     // 사용자 속성을 입력합니다.
     public static void setUserProperty(Dictionary<String,Object> keyValue) {
-
+        Log.d(TAG, "setUserProperty: ");
     }
 
     // 이벤트를 입력합니다.
-    public static void addEvent(String eventName, Dictionary<String, Object> keyValue) {
+    public static boolean addEvent(String eventName, String jsonValue) {
+        Log.d(TAG, "runAddEventThread: ");
+        Log.d(TAG, "addEvent: ");
+        String suffix = "/api/AddEvent";
+        String httpMethod = "POST";
+        String contentType = "application/json";
 
+        try {
+            URL url = new URL(DOMAIN+suffix);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod(httpMethod);
+            conn.setRequestProperty("Content-Type", contentType);
+            conn.setRequestProperty("Accept-Charset", "UTF-8");
+            conn.setConnectTimeout(10 * 1000);
+            conn.setReadTimeout(10 * 1000);
+
+            OutputStream os = conn.getOutputStream();
+            os.write(jsonValue.getBytes(StandardCharsets.UTF_8));
+            os.flush();
+
+            // 리턴된 결과 읽기
+            String inputLine;
+            StringBuilder outResult = new StringBuilder();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            while ((inputLine = in.readLine()) != null) {
+                outResult.append(inputLine);
+            }
+            readResponseCode(conn);
+            conn.disconnect();
+
+            JSONObject resultJson = new JSONObject(outResult.toString());
+            Log.d(TAG, "response : "+outResult);
+
+            return (boolean) resultJson.get("result");
+        } catch (Exception e) {
+            Log.w(TAG, "addEvent: "+e.getMessage(), e);
+        }
+
+        return false;
+    }
+
+    public static void getEvent(String appKey, int length) {
+        Log.d(TAG, "getEvent: ");
+        String host = "assignment.ad-brix.com";
+        String url = "/api/GetEvent";
+        String httpMethod = "post";
+        String contentType = "application/json";
+    }
+
+    private static void readResponseCode(HttpURLConnection connection){
+        try{
+            int responseCode = connection.getResponseCode();
+            if(responseCode == 400){
+                System.out.println("400 : command error");
+            } else if (responseCode == 500){
+                System.out.println("500 : Server error");
+            } else {
+                System.out.println("response code : " + responseCode);
+            }
+        } catch (IOException e){
+            Log.w(TAG, "readResponseCode: ", e);
+        }
     }
 }
--- iga_works_sdk/src/test/java/com/hig/iga_works_sdk/ExampleUnitTest.java
+++ iga_works_sdk/src/test/java/com/hig/iga_works_sdk/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package com.hig.iga_works_sdk;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +11,14 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void addEvent_isCorrect() {
+        String json = "{\"evt\":{\"created_at\":\"20221025115704\",\"event\":\"open_menu\",\"location\":{\"lat\":37.767,\"lng\":-122.42424},\"param\":{\"menu_name\":\"menu1\",\"menu_id\":30},\"user_properties\":{\"birthyear\":1982,\"gender\":\"m\",\"level\":37,\"character_class\":\"knight\",\"gold\":300}},\"common\":{\"identity\":{\"adid\":\"a5f21bc1-4a1d-48e0-8829-6dee007da8c7\",\"adid_opt_out\":false},\"device_info\":{\"os\":\"4.4.4\",\"model\":\"zerry_hp\",\"resolution\":\"1080x1920\",\"is_portrait\":false,\"platform\":\"android\",\"network\":\"wifi\",\"carrier\":\"VODAFONE+TR\",\"language\":\"ko\",\"country\":\"kr\"},\"package_name\":\"com.hig.iga_works\",\"appkey\":\"inqbator@naver.com\"}}";
+        assertTrue(IGASDK.addEvent("test_event", json));
+    }
+
+    @Test
+    public void addEventWithWrongAppKey_isCorrect() {
+        String json = "{\"evt\":{\"created_at\":\"20221025115704\",\"event\":\"open_menu\",\"location\":{\"lat\":37.767,\"lng\":-122.42424},\"param\":{\"menu_name\":\"menu1\",\"menu_id\":30},\"user_properties\":{\"birthyear\":1982,\"gender\":\"m\",\"level\":37,\"character_class\":\"knight\",\"gold\":300}},\"common\":{\"identity\":{\"adid\":\"a5f21bc1-4a1d-48e0-8829-6dee007da8c7\",\"adid_opt_out\":false},\"device_info\":{\"os\":\"4.4.4\",\"model\":\"zerry_hp\",\"resolution\":\"1080x1920\",\"is_portrait\":false,\"platform\":\"android\",\"network\":\"wifi\",\"carrier\":\"VODAFONE+TR\",\"language\":\"ko\",\"country\":\"kr\"},\"package_name\":\"com.hig.iga_works\",\"appkey\":\"inqbator@knou.ac.kr\"}}";
+        assertFalse(IGASDK.addEvent("test_event", json));
     }
 }
