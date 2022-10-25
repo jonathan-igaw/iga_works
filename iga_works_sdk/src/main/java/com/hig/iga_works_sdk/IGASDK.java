43dc103d145279a2490b6269849a92b084b4461a
hwang434 <inqbator@knou.ac.kr>
2022-10-25 23:08:33 +0900

    addEvent 동적으로 하게 변경, 테스트 코드 변경

--- iga_works_sdk/src/main/java/com/hig/iga_works_sdk/IGASDK.java
+++ iga_works_sdk/src/main/java/com/hig/iga_works_sdk/IGASDK.java
@@ -10,6 +10,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Dictionary;
+import java.util.Map;
+import java.util.Objects;
 
 public class IGASDK {
     private static final String TAG = "IGASDK";
@@ -21,17 +23,78 @@ public class IGASDK {
     }
 
     // 사용자 속성을 입력합니다.
-    public static void setUserProperty(Dictionary<String,Object> keyValue) {
+    public static void setUserProperty(Dictionary<String, String> keyValue) {
         Log.d(TAG, "setUserProperty: ");
     }
 
+    private static JSONObject makeAddEventBody(Map<String, String> map) {
+        Log.d(TAG, "makeAddEventBody: ");
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+            JSONObject evt = new JSONObject();
+            evt.put("created_at", map.get("created_at"));
+            evt.put("event", map.get("event"));
+
+            JSONObject locationJsonObject = new JSONObject();
+            locationJsonObject.put("lat", map.get("lat"));
+            locationJsonObject.put("lng", map.get("lng"));
+            evt.put("location", locationJsonObject);
+
+            // input by customer
+            JSONObject paramJsonObject = new JSONObject();
+            paramJsonObject.put("menu_name", map.get("menu_name"));
+            paramJsonObject.put("menu_id", map.get("menu_id"));
+            evt.put("param", paramJsonObject);
+
+            // Customer Info
+            JSONObject userPropertiesJsonObject = new JSONObject();
+            userPropertiesJsonObject.put("birthyear", map.get("birthyear"));
+            userPropertiesJsonObject.put("gender", map.get("gender"));
+            userPropertiesJsonObject.put("level", map.get("level"));
+            userPropertiesJsonObject.put("character_class", map.get("character_class"));
+            userPropertiesJsonObject.put("gold", map.get("gold"));
+            evt.put("user_properties", userPropertiesJsonObject);
+            jsonObject.put("evt", evt);
+
+            // common
+            JSONObject common = new JSONObject();
+            JSONObject identityJsonObject = new JSONObject();
+            identityJsonObject.put("adid", "a5f21bc1-4a1d-48e0-8829-6dee007da8c7");
+            identityJsonObject.put("adid_opt_out", false);
+
+            JSONObject deviceInfoObject = new JSONObject();
+            deviceInfoObject.put("os", map.get("os"));                // version of os
+            deviceInfoObject.put("model", map.get("model"));          // name of User Phone
+            deviceInfoObject.put("resolution", map.get("resolution"));    // current Display pixel
+            deviceInfoObject.put("is_portrait", map.get("is_portrait"));         // isPortrait?
+            deviceInfoObject.put("platform", map.get("platform"));        // Platform
+            deviceInfoObject.put("network", map.get("network"));            // Check NetworkStatus
+            deviceInfoObject.put("carrier", map.get("carrier"));     // Telecom company
+            deviceInfoObject.put("language", map.get("language"));             // Device Language
+            deviceInfoObject.put("country", map.get("country"));              // Device Country
+            common.put("identity", identityJsonObject);
+            common.put("device_info", deviceInfoObject);
+
+            String packageName = Objects.requireNonNull(IGASDK.class.getPackage()).getName();
+            common.put("package_name", packageName);
+            common.put("appkey", map.get("appkey"));
+            jsonObject.put("common", common);
+        } catch (Exception e) {
+            Log.w(TAG, "makeAddEventBody: "+e.getMessage(), e);
+        }
+
+        return jsonObject;
+    }
+
     // 이벤트를 입력합니다.
-    public static boolean addEvent(String eventName, String jsonValue) {
+    public static boolean addEvent(String eventName, Map<String, String> map) {
         Log.d(TAG, "runAddEventThread: ");
         Log.d(TAG, "addEvent: ");
         String suffix = "/api/AddEvent";
         String httpMethod = "POST";
         String contentType = "application/json";
+        String jsonValue = makeAddEventBody(map).toString();
 
         try {
             URL url = new URL(DOMAIN+suffix);
@@ -59,7 +122,6 @@ public class IGASDK {
 
             JSONObject resultJson = new JSONObject(outResult.toString());
             Log.d(TAG, "response : "+outResult);
-
             return (boolean) resultJson.get("result");
         } catch (Exception e) {
             Log.w(TAG, "addEvent: "+e.getMessage(), e);
