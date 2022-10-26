353b0eef48ea474cfa015910cd1cccc494344a54
hwang434 <inqbator@knou.ac.kr>
2022-10-27 00:24:38 +0900

    OnClickListener를 상속 받는 클래스 추가 의도 : 매 번 클릭 이벤트를 줄 때마다 사용자가 addEvent 메서드를 부를 필요 없게 하기 위해서 커스텀 클릭 리스너를 만들었다.
    코드가 하는 행동 : 클릭 이벤트가 발생하면 onClick의 매개변수로 받은 view의 아이디를 addEvent의 menuName 매개변수로 넘긴다. 그리고 addEvent를 새로운 스레드에서(ANR 방지) 실행한다.
    따라서, 앱 사용자는 map과 view만 넘기면 addEvent는 알아서 실행 된다.

--- app/src/main/java/com/hig/iga_works/MainActivity.java
+++ app/src/main/java/com/hig/iga_works/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.hig.iga_works_sdk.IGAMenuClickListener;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -13,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonMenu = findViewById(R.id.button_menu);
+        buttonMenu.setOnClickListener(new IGAMenuClickListener(TAG + " - Click Button") {
+            @Override
+            public void onClick(View view) {
+                super.onClick(view);
+                Log.d(TAG, "IGA Clicked Listener called: ");
+                Toast.makeText(MainActivity.this.getApplicationContext(), "button is clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
---
+++ iga_works_sdk/src/main/java/com/hig/iga_works_sdk/IGAMenuClickListener.java
@@ -0,0 +1,45 @@
+package com.hig.iga_works_sdk;
+
+import android.util.Log;
+import android.view.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class IGAMenuClickListener implements View.OnClickListener {
+    private static final String TAG = "IGAClickListener";
+    private final String event;
+
+    public IGAMenuClickListener(String event) {
+        this.event = event;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "onClick: ");
+        addEvent(view);
+    }
+
+    public void addEvent(View view) {
+        Log.d(TAG, "addEvent: ");
+        String menuName = view.getResources().getResourceName(view.getId());
+        int menuId = view.getId();
+        Map<String, Object> map = getEventMap(event, menuName, menuId);
+
+        new Thread() {
+            @Override
+            public void run() {
+                IGASDK.addEvent(event, map);
+            }
+        }.start();
+    }
+
+    private Map<String, Object> getEventMap(String event, String menuName, int menuId) {
+        Map<String, Object> mapForJson = new HashMap<>();
+        mapForJson.put("event", event);
+        mapForJson.put("menu_name", menuName);
+        mapForJson.put("menu_id", menuId);
+
+        return mapForJson;
+    }
+}
