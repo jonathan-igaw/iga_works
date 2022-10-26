6a04d24838165b433b93c95c849a44f8602005f7
hwang434 <inqbator@knou.ac.kr>
2022-10-26 23:43:29 +0900

    addEvent를 실행할 때, 디바이스의 정보를 AAR(SDK)에서 가져오게 변경 이전 코드 : 클라이언트의 어플리케이션에서 디바이스의 정보를 구한 후에 map에 담아서 addEvent() 메서드에 매개변수로 넘어줬다. 이후 코드 : IGASDK(AAR)에서 디바이스의 정보를 구한 후에 addEvent로 전달한다.

--- iga_works_sdk/src/main/java/com/hig/iga_works_sdk/IGASDK.java
+++ iga_works_sdk/src/main/java/com/hig/iga_works_sdk/IGASDK.java
@@ -1,6 +1,19 @@
 package com.hig.iga_works_sdk;
 
+import static android.content.Context.TELEPHONY_SERVICE;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import com.hig.iga_works_sdk.dto.UserInfo;
+import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,92 +22,182 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Dictionary;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
 public class IGASDK {
     private static final String TAG = "IGASDK";
     private static final String DOMAIN = "http://adbrix-sdk-assignment-backend-115895936.ap-northeast-1.elb.amazonaws.com";
+    private static String APP_KEY = "inqbator@naver.com";
+    private static Context applicationContext;
+    private static final UserInfo userInfo = new UserInfo();
 
     // SDK를 초기화 합니다. appkey가 필수적으로 필요합니다.
     public static void init(String appkey) {
         Log.d(TAG, "init: appkey = "+appkey);
+        APP_KEY = appkey;
     }
 
-    // 사용자 속성을 입력합니다.
-    public static void setUserProperty(Dictionary<String, String> keyValue) {
+    public static void setUserProperty(Map<String, Object> keyValue) {
         Log.d(TAG, "setUserProperty: ");
+        Integer birthYear = (Integer) keyValue.getOrDefault("birthyear", 0);
+        if (birthYear != null) userInfo.setBirthYear(birthYear);
+
+        String gender = (String) keyValue.getOrDefault("gender", "m");
+        if (gender != null) userInfo.setGender(gender);
+
+        Integer level = (Integer) keyValue.getOrDefault("level", 0);
+        if (level != null) userInfo.setLevel(level);
+
+        Integer gold = (Integer) keyValue.getOrDefault("gold", 0);
+        if (gold != null) userInfo.setGold(gold);
     }
 
-    private static JSONObject makeAddEventBody(Map<String, String> map) {
-        Log.d(TAG, "makeAddEventBody: ");
+    public static void setApplicationContext(Context context) {
+        Log.d(TAG, "setApplicationContext: context = "+context);
+        applicationContext = context;
+    }
+
+    private static JSONObject getAddEventJsonBody(String eventName, Map<String, Object> map) {
+        Log.d(TAG, "getAddEventJsonBody: ");
         JSONObject jsonObject = new JSONObject();
+        if (map == null) {
+            setEvtContent(eventName, jsonObject);
+        } else {
+            setEvtContent(eventName, jsonObject, map);
+        }
+        setCommonContent(jsonObject);
+
+        return jsonObject;
+    }
+
+    private static void setUserPropertiesJsonObject(JSONObject userPropertiesJsonObject) {
+        Log.d(TAG, "setUserPropertiesJsonObject: ");
+        // user_properties
+        try {
+            userPropertiesJsonObject.put("birthyear", userInfo.getBirthYear());
+            userPropertiesJsonObject.put("gender", userInfo.getGender());
+            userPropertiesJsonObject.put("level", userInfo.getLevel());
+            userPropertiesJsonObject.put("character_class", userInfo.getCharacterClass());
+            userPropertiesJsonObject.put("gold", userInfo.getGold());
+        } catch (JSONException e) {
+            Log.w(TAG, "putUserInfoToJsonObject: "+e.getMessage(), e);
+        }
+    }
+
+    private static void setEvtContent(String eventName, JSONObject jsonObject) {
+        Log.d(TAG, "setEvtContent: ");
+        JSONObject evt = new JSONObject();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault());
+        String createdAt = sdf.format(System.currentTimeMillis());
 
         try {
-            JSONObject evt = new JSONObject();
-            evt.put("created_at", map.get("created_at"));
-            evt.put("event", map.get("event"));
+            evt.put("created_at", createdAt);
+            evt.put("event", eventName);
 
-            JSONObject locationJsonObject = new JSONObject();
-            locationJsonObject.put("lat", map.get("lat"));
-            locationJsonObject.put("lng", map.get("lng"));
-            evt.put("location", locationJsonObject);
+            // param
+            JSONObject paramJsonObject = new JSONObject();
+            paramJsonObject.put("menu_name", eventName);
+            paramJsonObject.put("menu_id", "menu_id");
+            evt.put("param", paramJsonObject);
 
-            // input by customer
+            // user_properties
+            JSONObject userPropertiesJsonObject = new JSONObject();
+            setUserPropertiesJsonObject(userPropertiesJsonObject);
+            evt.put("user_properties", userPropertiesJsonObject);
+            jsonObject.put("evt", evt);
+            evt.put("user_properties", userPropertiesJsonObject);
+            jsonObject.put("evt", evt);
+        } catch (JSONException e) {
+            Log.w(TAG, "putEvtContent: "+e.getMessage(), e);
+        }
+    }
+
+    private static void setEvtContent(String eventName, JSONObject jsonObject, Map<String, Object> map) {
+        Log.d(TAG, "setEvtContent: ");
+        JSONObject evt = new JSONObject();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault());
+        String createdAt = sdf.format(System.currentTimeMillis());
+
+        try {
+            evt.put("created_at", createdAt);
+            evt.put("event", eventName);
+
+            Double lat = (Double) map.getOrDefault("lat",Double.MIN_VALUE);
+            Double lng = (Double) map.getOrDefault("lat",Double.MIN_VALUE);
+            // if lat or lng is Double.MIN_VALUE, then location is null
+            if (lat != null && lat != Double.MIN_VALUE && lng != null && lng != Double.MIN_VALUE) {
+                JSONObject locationJsonObject = new JSONObject();
+                locationJsonObject.put("lat", lat);
+                locationJsonObject.put("lng", lng);
+                evt.put("location", locationJsonObject);
+            }
+
+            // param
             JSONObject paramJsonObject = new JSONObject();
             paramJsonObject.put("menu_name", map.get("menu_name"));
             paramJsonObject.put("menu_id", map.get("menu_id"));
             evt.put("param", paramJsonObject);
 
-            // Customer Info
+            // user_properties
             JSONObject userPropertiesJsonObject = new JSONObject();
-            userPropertiesJsonObject.put("birthyear", map.get("birthyear"));
-            userPropertiesJsonObject.put("gender", map.get("gender"));
-            userPropertiesJsonObject.put("level", map.get("level"));
-            userPropertiesJsonObject.put("character_class", map.get("character_class"));
-            userPropertiesJsonObject.put("gold", map.get("gold"));
+            setUserPropertiesJsonObject(userPropertiesJsonObject);
             evt.put("user_properties", userPropertiesJsonObject);
             jsonObject.put("evt", evt);
+            evt.put("user_properties", userPropertiesJsonObject);
+            jsonObject.put("evt", evt);
+        } catch (JSONException e) {
+            Log.w(TAG, "putEvtContent: "+e.getMessage(), e);
+        }
+    }
 
+    private static void setCommonContent(JSONObject jsonObject) {
+        Log.d(TAG, "setCommonContent: ");
+        try {
             // common
             JSONObject common = new JSONObject();
+
+            // identity
             JSONObject identityJsonObject = new JSONObject();
             identityJsonObject.put("adid", "a5f21bc1-4a1d-48e0-8829-6dee007da8c7");
             identityJsonObject.put("adid_opt_out", false);
 
+            // deviceInfo
             JSONObject deviceInfoObject = new JSONObject();
-            deviceInfoObject.put("os", map.get("os"));                // version of os
-            deviceInfoObject.put("model", map.get("model"));          // name of User Phone
-            deviceInfoObject.put("resolution", map.get("resolution"));    // current Display pixel
-            deviceInfoObject.put("is_portrait", map.get("is_portrait"));         // isPortrait?
-            deviceInfoObject.put("platform", map.get("platform"));        // Platform
-            deviceInfoObject.put("network", map.get("network"));            // Check NetworkStatus
-            deviceInfoObject.put("carrier", map.get("carrier"));     // Telecom company
-            deviceInfoObject.put("language", map.get("language"));             // Device Language
-            deviceInfoObject.put("country", map.get("country"));              // Device Country
+            deviceInfoObject.put("os", Build.VERSION.SDK_INT);                // version of os
+            deviceInfoObject.put("model", Build.MODEL);          // name of User Phone
+            int[] widthAndHeight = getResolution();
+            deviceInfoObject.put("resolution", widthAndHeight[0]+"x"+widthAndHeight[1]);    // current Display pixel
+            deviceInfoObject.put("is_portrait", isPortrait());         // isPortrait?
+            deviceInfoObject.put("platform", "android");        // Platform
+            deviceInfoObject.put("network", getNetworkStatus());            // Check NetworkStatus
+            deviceInfoObject.put("carrier", getNetworkOperatorName());     // Telecom company
+
+            deviceInfoObject.put("language", Locale.getDefault().getLanguage());             // Device Language
+            deviceInfoObject.put("country", Locale.getDefault().getCountry());              // Device Country
             common.put("identity", identityJsonObject);
             common.put("device_info", deviceInfoObject);
 
+            // package and appkey
             String packageName = Objects.requireNonNull(IGASDK.class.getPackage()).getName();
             common.put("package_name", packageName);
-            common.put("appkey", map.get("appkey"));
+            common.put("appkey", APP_KEY);
             jsonObject.put("common", common);
-        } catch (Exception e) {
-            Log.w(TAG, "makeAddEventBody: "+e.getMessage(), e);
+        } catch (JSONException e) {
+            Log.w(TAG, "setCommonContent: "+e.getMessage(), e);
         }
-
-        return jsonObject;
     }
 
     // 이벤트를 입력합니다.
-    public static boolean addEvent(String eventName, Map<String, String> map) {
-        Log.d(TAG, "runAddEventThread: ");
+    public static boolean addEvent(String eventName, Map<String, Object> map) {
         Log.d(TAG, "addEvent: ");
         String suffix = "/api/AddEvent";
         String httpMethod = "POST";
         String contentType = "application/json";
-        String jsonValue = makeAddEventBody(map).toString();
+        String jsonValue = getAddEventJsonBody(eventName, map).toString();
+        Log.d(TAG, "request jsonValue : "+jsonValue);
 
         try {
             URL url = new URL(DOMAIN+suffix);
@@ -151,5 +254,47 @@ public class IGASDK {
         } catch (IOException e){
             Log.w(TAG, "readResponseCode: ", e);
         }
+    }
+
+    private static int[] getResolution() {
+        Log.d(TAG, "getResolution: applicationContext : "+applicationContext);
+        WindowManager wm = (WindowManager) applicationContext.getSystemService(
+                Context.WINDOW_SERVICE
+        );
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+        return new int[] {width, height};
+    }
+
+    private static boolean isPortrait() {
+        return applicationContext.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+
+    private static String getNetworkStatus() {
+        ConnectivityManager connMgr = (ConnectivityManager) applicationContext.getSystemService(
+                Context.CONNECTIVITY_SERVICE
+        );
+        for (Network network : connMgr.getAllNetworks()) {
+            NetworkInfo networkInfo = connMgr.getNetworkInfo(network);
+            if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI && networkInfo.isConnected()) {
+                return "wifi";
+            }
+            if (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE && networkInfo.isConnected()) {
+                return "mobile";
+            }
+        }
+
+        return null;
+    }
+
+    private static String getNetworkOperatorName() {
+        TelephonyManager tm = (TelephonyManager) applicationContext.getSystemService(
+                TELEPHONY_SERVICE
+        );
+        return tm.getNetworkOperatorName();
     }
 }
